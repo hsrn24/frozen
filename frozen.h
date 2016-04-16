625667a775585de0ca26a8d90bf3e989ceef74cb
Krystian Dużyński <krystian.duzynski@gmail.com>
2016-04-16 03:05:12 +0200

    added callback-based format type for base64, removed unused code

--- frozen.h
+++ frozen.h
@@ -26,33 +26,7 @@ extern "C" {
 
 #include <stdarg.h>
 
-enum json_type {
-  JSON_TYPE_EOF = 0, /* End of parsed tokens marker */
-  JSON_TYPE_STRING = 1,
-  JSON_TYPE_NUMBER = 2,
-  JSON_TYPE_OBJECT = 3,
-  JSON_TYPE_TRUE = 4,
-  JSON_TYPE_FALSE = 5,
-  JSON_TYPE_NULL = 6,
-  JSON_TYPE_ARRAY = 7
-};
-
-struct json_token {
-  const char *ptr;     /* Points to the beginning of the token */
-  int len;             /* Token length */
-  int num_desc;        /* For arrays and object, total number of descendants */
-  enum json_type type; /* Type of the token, possible values above */
-};
-
-/* Error codes */
-#define JSON_STRING_INVALID -1
-#define JSON_STRING_INCOMPLETE -2
-#define JSON_TOKEN_ARRAY_TOO_SMALL -3
-
-int parse_json(const char *json_string, int json_string_length,
-               struct json_token *tokens_array, int size_of_tokens_array);
-struct json_token *parse_json2(const char *json_string, int string_length);
-struct json_token *find_json_token(struct json_token *toks, const char *path);
+typedef char (*frozen_fun_t)(void*);
 
 int json_emit_long(char *buf, int buf_len, long value);
 int json_emit_double(char *buf, int buf_len, double value);
